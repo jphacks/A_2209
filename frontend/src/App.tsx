789ace15564f96489b37dd2ed41c3f5217922033
Sippo <oppis02.anzu@gmail.com>
2022-10-21 15:50:17 +0900

    display-map

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useCallback, useEffect,createContext } from "react";
+import { Loader } from "@googlemaps/js-api-loader"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App:React.FC = () =>{
+  
+  return <Map/>
 }
 
-export default App;
+type Props = {
+  googleMap:google.maps.Map|null;
+};
+
+const GoogleMapsContext = createContext<Props>({
+  googleMap:null
+  }
+)
+
+const GoogleMaps = (
+) =>{
+  const [googleMap,setGoogleMap] = useState<google.maps.Map|null>(null);
+  const initGoogleMaps = useCallback(()=>{
+    const map = new google.maps.Map(document.querySelector("#map")!,{
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+    setGoogleMap(map);
+  },[])
+  
+  useEffect(()=>{
+    initGoogleMaps();
+  },[]);
+
+  return (
+    <>
+      <div id='map' style={{height:"1000px"}}/>
+
+      <GoogleMapsContext.Provider value={{
+        googleMap:googleMap
+      }}>
+      </GoogleMapsContext.Provider>
+    </>
+  )
+
+}
+
+const Map:React.FC = () =>{
+  const [googleMapsApiLoaded,setgoogleMapsApiLoaded] = useState<boolean>(false);
+
+  const initGoogleMapsApi:any = useCallback(async ()=>{
+    const loader = new Loader({
+      apiKey: "",
+      version:'weekly'
+    });
+    console.log("now loading")
+    await loader.load();
+    console.log("load completeed")
+    setgoogleMapsApiLoaded(true)
+
+  },[])
+
+  initGoogleMapsApi()
+
+  
+  return (
+    <>
+      {googleMapsApiLoaded && (
+        <GoogleMaps/>
+      )}
+    </>
+  );
+} 
+
+export default App 
