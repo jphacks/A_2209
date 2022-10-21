41afa44cf78536c716a78e93cd79634b9f0f2838
sugarasot <euphonioussilvertuba@gmail.com>
2022-10-22 03:28:04 +0900

    自動向き更新対応

---
+++ idokeido_html5_2/script_audioPlayer.js
@@ -0,0 +1,35 @@
+// CORSエラー回避: about:configからsecurity.fileuri.strict_origin_policyをfalseに
+class AudioPlayer {
+    constructor() {
+        this.audioContext = new AudioContext();
+        this.audioElement = new Audio();
+        this.audioElement.loop = true;
+        this.audioSource = this.audioContext.createMediaElementSource(this.audioElement);
+        this.panner = new NavigatorPanner(this.audioContext);
+
+        this.audioSource.connect(this.panner);
+        this.panner.connect(this.audioContext.destination);
+    }
+    setCurrentCoordinate(_latitude, _longitude) {
+        this.panner.setCurrentCoordinate(_latitude, _longitude);
+    }
+    setDestinationCoordinate(_latitude, _longitude) {
+        this.panner.setDestinationCoordinate(_latitude, _longitude);
+    }
+    setHeading(_heading) {
+        this.panner.setHeading(_heading);
+    }
+    setAudioURL(fileUrl) {
+        this.audioElement.src = fileUrl;
+    }
+    play() {
+        this.audioElement.play();
+    }
+    pause() {
+        this.audioElement.pause();
+    }
+    stop() {
+        this.audioElement.pause();
+        this.audioElement.currentTime = 0;
+    }
+}
