04a0a3bc4170801251fce2cf72f15d4764066bcf
USK314 <usk31101021@gmail.com>
2022-11-13 01:11:41 +0900

    feat: 位置情報まわりのエラー処理

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -5,71 +5,88 @@ import Drawer from '@mui/material/Drawer';
 import Fade from '@mui/material/Fade';
 import Grow from '@mui/material/Grow';
 import Slide from '@mui/material/Slide';
-import {Button, IconButton, FormControlLabel} from '@mui/material';
+import { Button, IconButton, FormControlLabel } from '@mui/material';
 import LoginIcon from '@mui/icons-material/Login';
 import CloseIcon from '@mui/icons-material/Close';
 import PersonIcon from '@mui/icons-material/Person';
 import AudioPlayer from "../utils/script_audioPlayer";
 import { CSSTransition } from 'react-transition-group';
 
-import {Signin} from '../components/signin'
-import {Signup} from '../components/signup'
+import { Signin } from '../components/signin'
+import { Signup } from '../components/signup'
 import { UserUtils } from "../components/userUtils";
 // import { useShadePressed, useLoginPressed, useSignupPressed } from "../hooks/hooks";
 import { pressedType, Pressed } from '../contexts/contexts'
 
 import '../css/home.css'
 
-const Home:React.FC = () =>{
+const Home: React.FC = () => {
 
-  return <Map/>
+  return <Map />
 }
 
 type Props = {
-  googleMap:google.maps.Map|null;
+  googleMap: google.maps.Map | null;
 };
 
 const GoogleMapsContext = createContext<Props>({
-  googleMap:null
-  }
+  googleMap: null
+}
 )
 
 type markerInfo = {
-  position:google.maps.LatLng;title:string
+  position: google.maps.LatLng; title: string
 }
 
 
 
-const Markers:React.FC = ()=>{
-  const {googleMap} = useContext(GoogleMapsContext);
+const Markers: React.FC = () => {
+  const { googleMap } = useContext(GoogleMapsContext);
 
-  let my_pos:markerInfo
-  if(navigator.geolocation){
-    let pos:google.maps.LatLng
-    navigator.geolocation.getCurrentPosition((position:GeolocationPosition)=>{
-    pos = new google.maps.LatLng({lat:position.coords.latitude,lng:position.coords.longitude})
-    my_pos = {position:pos,title:"yourplace"}
-    googleMap?.setCenter(pos)
-    make_marker()
-    })
+  let my_pos: markerInfo
+  if (navigator.geolocation) {
+    let pos: google.maps.LatLng
+    navigator.geolocation.getCurrentPosition(
+      (position: GeolocationPosition) => {
+        pos = new google.maps.LatLng({ lat: position.coords.latitude, lng: position.coords.longitude })
+        my_pos = { position: pos, title: "yourplace" }
+        googleMap?.setCenter(pos)
+        make_marker()
+      }, 
+      (error: GeolocationPositionError) => {
+        switch(error.code) {
+          case 1:
+            alert("位置情報の利用が許可されていません\nブラウザの設定から位置情報の共有を許可してください");
+            break;
+          case 2:
+            alert("現在位置が取得できませんでした");
+            break;
+          case 3:
+            alert("タイムアウトになりました");
+            break;
+          default:
+            alert("その他のエラー(エラーコード："+error.code+")");
+            break;
+        }
+      })
   }
 
   const infowindow = new google.maps.InfoWindow()
 
-  const make_marker = () =>{
-      const marker = new google.maps.Marker({
-        position:my_pos["position"],
-        title:my_pos["title"],
-        map:googleMap,
-        icon:"me.png",
-        animation:google.maps.Animation.DROP
+  const make_marker = () => {
+    const marker = new google.maps.Marker({
+      position: my_pos["position"],
+      title: my_pos["title"],
+      map: googleMap,
+      icon: "me.png",
+      animation: google.maps.Animation.DROP
 
-      })
-      marker.addListener("click",()=>{
-        infowindow.close();
-        infowindow.setContent(marker.getTitle());
-        infowindow.open(marker.getMap(),marker)
-      })
+    })
+    marker.addListener("click", () => {
+      infowindow.close();
+      infowindow.setContent(marker.getTitle());
+      infowindow.open(marker.getMap(), marker)
+    })
   }//end of make_marker
 
 
@@ -86,43 +103,43 @@ const Markers:React.FC = ()=>{
 }
 
 const GoogleMaps = (
-  props:any
-) =>{
-  const {children} = props
-  const [googleMap,setGoogleMap] = useState<google.maps.Map|null>(null);
-  const [destiTitle,setDestiTitle] = useState<string>("");
-  const [destiPosition,setDestionPosition] = useState<google.maps.LatLng>(new google.maps.LatLng({lat:0,lng:0}))
-  const [destiMarker,setDestiMarker] = useState<google.maps.Marker>(new google.maps.Marker(null))
-  const [showModal,setShowModal] = useState<boolean>(false)
+  props: any
+) => {
+  const { children } = props
+  const [googleMap, setGoogleMap] = useState<google.maps.Map | null>(null);
+  const [destiTitle, setDestiTitle] = useState<string>("");
+  const [destiPosition, setDestionPosition] = useState<google.maps.LatLng>(new google.maps.LatLng({ lat: 0, lng: 0 }))
+  const [destiMarker, setDestiMarker] = useState<google.maps.Marker>(new google.maps.Marker(null))
+  const [showModal, setShowModal] = useState<boolean>(false)
   //audioタグの状態管理　0:startしてない 1:一時停止中 2:再生中
-  const[isPlaying,setIsPlaying] = useState<Number>(0)
-  const [isAudioCreated,setIsAudioCreated] = useState<boolean>(false)
-  const apRef:any = useRef(null)
+  const [isPlaying, setIsPlaying] = useState<Number>(0)
+  const [isAudioCreated, setIsAudioCreated] = useState<boolean>(false)
+  const apRef: any = useRef(null)
 
 
 
 
-  const initGoogleMaps = useCallback(()=>{
-    const map = new google.maps.Map(document.querySelector("#map")!,{
+  const initGoogleMaps = useCallback(() => {
+    const map = new google.maps.Map(document.querySelector("#map")!, {
       center: { lat: -25.344, lng: 131.031 },
       zoom: 8,
       fullscreenControl: false,
       streetViewControl: false,
-      zoomControl:false,
+      zoomControl: false,
       mapTypeControl: false,
     });
     setGoogleMap(map);
-  },[])
+  }, [])
 
-  let searchBox:google.maps.places.SearchBox ;
-  useEffect(()=>{
+  let searchBox: google.maps.places.SearchBox;
+  useEffect(() => {
     const input_tag = document.getElementById("search-box") as HTMLInputElement;
     searchBox = new google.maps.places.SearchBox(input_tag);
     // googleMap?.controls[google.maps.ControlPosition.TOP_CENTER].push(input_tag);
 
-    searchBox.addListener("places_changed",()=>{
+    searchBox.addListener("places_changed", () => {
       const places = searchBox.getPlaces();
-      if (places?.length != 1){
+      if (places?.length != 1) {
         alert("2つ以上の目的地を設定することはできません")
         return;
       }
@@ -136,59 +153,59 @@ const GoogleMaps = (
 
 
 
-  },[]);
+  }, []);
 
-  const stopAudio = ()=>{
+  const stopAudio = () => {
     const aud = document.getElementById("audio") as HTMLAudioElement;
     aud.pause()
     setIsPlaying(1)
   }
 
-  const endAudio = ()=>{
+  const endAudio = () => {
     const aud = document.getElementById("audio") as HTMLAudioElement;
     aud.pause()
     setIsPlaying(0)
   }
 
-  const startAudio = ()=>{
+  const startAudio = () => {
     const aud = document.getElementById("audio") as HTMLAudioElement;
     aud.play()
     setIsPlaying(2)
   }
 
-  const handleClick = ()=>{
+  const handleClick = () => {
     console.log("handleClick")
-    if(!isAudioCreated){
+    if (!isAudioCreated) {
       apRef.current = new AudioPlayer();
       setIsAudioCreated(true)
     }
 
-    function success(pos:any) {
+    function success(pos: any) {
       const curLoc = pos.coords;
       apRef.current.setCurrentCoordinate(curLoc.latitude, curLoc.longitude);
-  }
-    function error(err:any) {
+    }
+    function error(err: any) {
       console.warn('ERROR(' + err.code + '): ' + err.message);
-  }
+    }
     function detectOSSimply() {
       let ret;
       if (
-          navigator.userAgent.indexOf("iPhone") > 0 ||
-          navigator.userAgent.indexOf("iPad") > 0 ||
-          navigator.userAgent.indexOf("iPod") > 0
+        navigator.userAgent.indexOf("iPhone") > 0 ||
+        navigator.userAgent.indexOf("iPad") > 0 ||
+        navigator.userAgent.indexOf("iPod") > 0
       ) {
-          // iPad OS13のsafariはデフォルト「Macintosh」なので別途要対応
-          ret = "iphone";
+        // iPad OS13のsafariはデフォルト「Macintosh」なので別途要対応
+        ret = "iphone";
       } else if (navigator.userAgent.indexOf("Android") > 0) {
-          ret = "android";
+        ret = "android";
       } else {
-          ret = "pc";
+        ret = "pc";
       }
 
       return ret;
-  }
+    }
 
-    function compassHeading(alpha:any, beta:any, gamma:any) {
+    function compassHeading(alpha: any, beta: any, gamma: any) {
       var degtorad = Math.PI / 180; // Degree-to-Radian conversion
 
       var _x = beta ? beta * degtorad : 0; // beta value
@@ -211,31 +228,31 @@ const GoogleMaps = (
 
       // Convert compass heading to use whole unit circle
       if (Vy < 0) {
-          compassHeading += Math.PI;
+        compassHeading += Math.PI;
       } else if (Vx < 0) {
-          compassHeading += 2 * Math.PI;
+        compassHeading += 2 * Math.PI;
       }
 
       return compassHeading * (180 / Math.PI); // Compass Heading (in degrees)
-  }
+    }
 
-  function watchHeadingiPhone(event:any) {
-    const degrees = 360 - event.webkitCompassHeading;
-    apRef.current.setHeading(degrees);
-  }
+    function watchHeadingiPhone(event: any) {
+      const degrees = 360 - event.webkitCompassHeading;
+      apRef.current.setHeading(degrees);
+    }
 
-  function watchHeadingAndroid(event:any) {
-    const alpha = event.alpha;
-    const beta = event.beta;
-    const gamma = event.gamma;
-    if (alpha === null) {
+    function watchHeadingAndroid(event: any) {
+      const alpha = event.alpha;
+      const beta = event.beta;
+      const gamma = event.gamma;
+      if (alpha === null) {
         window.removeEventListener("deviceorientationabsolute", watchHeadingAndroid);
         alert("deviceorientationabsolute非対応");
         return;
+      }
+      const degrees = 360 - compassHeading(alpha, beta, gamma);
+      apRef.curent.setHeading(degrees);
     }
-    const degrees = 360 - compassHeading(alpha, beta, gamma);
-    apRef.curent.setHeading(degrees);
-}
 
 
     const options = {
@@ -244,25 +261,25 @@ const GoogleMaps = (
       maximumAge: 0
     };
 
-    apRef.current.setDestinationCoordinate(destiPosition.lat(),destiPosition.lng())
+    apRef.current.setDestinationCoordinate(destiPosition.lat(), destiPosition.lng())
     // ap.setAudioURL("music.ogg")
-    const watch_position_id = navigator.geolocation.watchPosition(success,error,options);
+    const watch_position_id = navigator.geolocation.watchPosition(success, error, options);
     const OS = detectOSSimply();
 
-    if(OS == "iphone") {
+    if (OS == "iphone") {
       // iPhone + Safariの場合はDeviceOrientation APIの使用許可をユーザに求める
       (DeviceOrientationEvent as any).requestPermission()
-          .then((response:any) => {
-              if (response === "granted") {
-                  window.addEventListener("deviceorientation", watchHeadingiPhone);
-              }
-          })
-          .catch((err:any) => {console.log(err)});
-    }else if(OS == "android") {
-        window.addEventListener("deviceorientationabsolute", watchHeadingAndroid);
-        //window.addEventListener("deviceorientation", watchHeadingAndroid);
-    }else {
-        alert("お使いの環境はこのプロダクトに対応していない可能性があります！");
+        .then((response: any) => {
+          if (response === "granted") {
+            window.addEventListener("deviceorientation", watchHeadingiPhone);
+          }
+        })
+        .catch((err: any) => { console.log(err) });
+    } else if (OS == "android") {
+      window.addEventListener("deviceorientationabsolute", watchHeadingAndroid);
+      //window.addEventListener("deviceorientation", watchHeadingAndroid);
+    } else {
+      alert("お使いの環境はこのプロダクトに対応していない可能性があります！");
     }
 
     apRef.current.play();
@@ -271,25 +288,25 @@ const GoogleMaps = (
   }
 
 
-  useEffect(()=>{
+  useEffect(() => {
     destiMarker.setMap(null)
     setDestiMarker(
       new google.maps.Marker({
-        map:googleMap,
-        title:destiTitle,
-        position:destiPosition,
-        animation:google.maps.Animation.DROP
+        map: googleMap,
+        title: destiTitle,
+        position: destiPosition,
+        animation: google.maps.Animation.DROP
       }))
-      let response = googleMap?.setCenter( destiPosition ) ;
-      console.log(response)
-      googleMap?.setCenter(destiPosition)
+    let response = googleMap?.setCenter(destiPosition);
+    console.log(response)
+    googleMap?.setCenter(destiPosition)
 
-  },[destiTitle,destiPosition])
+  }, [destiTitle, destiPosition])
 
 
-  useEffect(()=>{
+  useEffect(() => {
     initGoogleMaps();
-  },[]);
+  }, []);
 
   // const {shadePressed, shadePressedtoFalse, shadePressedtoTrue} = useShadePressed();
   // const {loginPressed, loginPressedtoFalse, loginPressedtoTrue} = useLoginPressed();
@@ -298,48 +315,48 @@ const GoogleMaps = (
 
   return (
 
-    <div style={{height:window.innerHeight}}>
+    <div style={{ height: window.innerHeight }}>
 
       <Box sx={{
-          width: "100%",
-          textAlign:"center",
-          height: 80,
-          backgroundColor: 'primary.dark',
-          left: "50%",
-          }}
-          alignItems="center"
-          justifyContent="center"
-          position="absolute"
-          zIndex={1000}
-          className="header"
-          left={0}
-          top={0}
-        >
+        width: "100%",
+        textAlign: "center",
+        height: 80,
+        backgroundColor: 'primary.dark',
+        left: "50%",
+      }}
+        alignItems="center"
+        justifyContent="center"
+        position="absolute"
+        zIndex={1000}
+        className="header"
+        left={0}
+        top={0}
+      >
         <input id="search-box" style={{
-          backgroundColor:"#fff",
+          backgroundColor: "#fff",
           fontFamily: "Roboto",
           fontSize: "25px",
           fontWeight: "300m",
           marginLeft: "12px",
-          padding:" 0 11px 0 13px",
+          padding: " 0 11px 0 13px",
           textOverflow: "ellipsis",
-          width:"60%",
-          height:"80%",
-          margin:"0%",
-          marginTop:'4px',
-          display:'inline'
-        }}/>
+          width: "60%",
+          height: "80%",
+          margin: "0%",
+          marginTop: '4px',
+          display: 'inline'
+        }} />
       </Box>
 
-      <div id='map' style={{height:window.innerHeight}}/>
+      <div id='map' style={{ height: window.innerHeight }} />
       {
-        isPlaying==2?(
-          <Button id="musicStopper" color='secondary' variant="contained" style={{position:"absolute",bottom:"10px",left:'10px',width:'100px',height:'100px'}} onClick={stopAudio}>停止</Button>
-          ):null}
+        isPlaying == 2 ? (
+          <Button id="musicStopper" color='secondary' variant="contained" style={{ position: "absolute", bottom: "10px", left: '10px', width: '100px', height: '100px' }} onClick={stopAudio}>停止</Button>
+        ) : null}
       {
-        isPlaying==1?(
-          <Button id="musicStopper" color='primary' variant="contained" style={{position:"absolute",bottom:"10px",left:'10px',width:'100px',height:'100px'}} onClick={startAudio}>再生</Button>
-      ):null}
+        isPlaying == 1 ? (
+          <Button id="musicStopper" color='primary' variant="contained" style={{ position: "absolute", bottom: "10px", left: '10px', width: '100px', height: '100px' }} onClick={startAudio}>再生</Button>
+        ) : null}
 
 
       <IconButton
@@ -377,11 +394,11 @@ const GoogleMaps = (
       >
         <div
           className="shade"
-          // onClick={() => {
-          //   isPressed.setShade(false);
-          //   isPressed.setSignin(false);
-          //   isPressed.setSignup(false);
-          // }}
+        // onClick={() => {
+        //   isPressed.setShade(false);
+        //   isPressed.setSignin(false);
+        //   isPressed.setSignup(false);
+        // }}
         >
           <Grow
             unmountOnExit
@@ -389,7 +406,7 @@ const GoogleMaps = (
             className="signin signWrapper"
           >
             <div>
-              <Signin/>
+              <Signin />
             </div>
           </Grow>
 
@@ -399,7 +416,7 @@ const GoogleMaps = (
             className="signup  signWrapper"
           >
             <div>
-              <Signup/>
+              <Signup />
             </div>
           </Grow>
 
@@ -419,13 +436,13 @@ const GoogleMaps = (
       </Fade>
 
       <Drawer
-      anchor="bottom"
-      open={showModal}
-      onClose={()=>{setShowModal(false)}}
-      onClick={()=>{setShowModal(false)}}
-      style={{textAlign:"center"}}
+        anchor="bottom"
+        open={showModal}
+        onClose={() => { setShowModal(false) }}
+        onClick={() => { setShowModal(false) }}
+        style={{ textAlign: "center" }}
       >
-        <Box style={{margin:"10px"}}>
+        <Box style={{ margin: "10px" }}>
           <p>{destiTitle}</p>
           <Box>
             <Button variant="outlined" size='large' onClick={handleClick}>出発する</Button>
@@ -435,7 +452,7 @@ const GoogleMaps = (
 
 
       <GoogleMapsContext.Provider value={{
-        googleMap:googleMap
+        googleMap: googleMap
       }}>
         {children}
       </GoogleMapsContext.Provider>
@@ -444,21 +461,21 @@ const GoogleMaps = (
 
 }
 
-const Map:React.FC = () =>{
+const Map: React.FC = () => {
   const key = process.env["REACT_APP_MAP_API_KEY"]!
-  const [googleMapsApiLoaded,setgoogleMapsApiLoaded] = useState<boolean>(false);
+  const [googleMapsApiLoaded, setgoogleMapsApiLoaded] = useState<boolean>(false);
 
-  const initGoogleMapsApi:any = useCallback(async ()=>{
+  const initGoogleMapsApi: any = useCallback(async () => {
     const loader = new Loader({
       apiKey: key,
-      version:'weekly',
-      libraries:["places"]
+      version: 'weekly',
+      libraries: ["places"]
 
     });
     await loader.load();
     setgoogleMapsApiLoaded(true)
 
-  },[])
+  }, [])
 
   initGoogleMapsApi()
 
@@ -467,9 +484,9 @@ const Map:React.FC = () =>{
     <>
       {googleMapsApiLoaded && (
         <div>
-        <GoogleMaps>
-          <Markers/>
-        </GoogleMaps>
+          <GoogleMaps>
+            <Markers />
+          </GoogleMaps>
         </div>
       )}
     </>
